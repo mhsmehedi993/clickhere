9e8ba8d20164988bb6d3cca69207e393b9160ef7
mhsmehedi993 <147188821+mhsmehedi993@users.noreply.github.com>
2023-10-07 11:19:33 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://clickhere.wuaze.com/graphql"
+	const endpoint = "https://clickhere.wuaze.com/gql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
